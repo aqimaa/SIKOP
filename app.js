a9b7844ce5dd3ce064ffdc04ae0d5bf42f40fec5
Meydiva-IY <meydivaintayeza@gmail.com>
2025-01-24 10:37:01 +0700

    Route Edit

--- app.js
+++ app.js
@@ -4,22 +4,10 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-// Database Configuration
-const database = require('./config/database'); // Pastikan file database sudah diatur dengan benar
-
-// Route Imports
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const authRoutes = require('./routes/authRoutes');
-const masterRoutes = require('./routes/masterRoutes');
-const simpananRoutes = require('./routes/simpananRoutes');
-const pinjamanRoutes = require('./routes/pinjamanRoutes');
-const kreditRoutes = require('./routes/kreditRoutes');
-const laporanRoutes = require('./routes/laporanRoutes');
+const allRoutes = require('./route/indexRoutes.js');
 
 // Initialize app
 const app = express();
@@ -42,36 +30,29 @@ app.use(
 );
 app.use(flash());
 
-// Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/auth', authRoutes);
-app.use('/master', masterRoutes);
-app.use('/simpanan', simpananRoutes);
-app.use('/pinjaman', pinjamanRoutes);
-app.use('/kredit', kreditRoutes);
-app.use('/laporan', laporanRoutes);
-
 // Default Home Route
 app.get('/', (req, res) => {
-  res.redirect('/auth/login');
+  res.redirect('/auth/login'); // Redirect ke halaman login
 });
 
-// Catch 404 and Forward to Error Handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+// Routes
+app.use(allRoutes);
 
-// Error Handler
-app.use((err, req, res, next) => {
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // Catch 404 and Forward to Error Handler
+// app.use((req, res, next) => {
+//   next(createError(404));
+// });
 
-  // Render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // Error Handler
+// app.use((err, req, res, next) => {
+//   // Set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // Render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 // Handle 404 errors
 app.use('*', (req, res) => {
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Import Controllers
+const loginController = require("../controller/auth/loginController.js");
+const laporanController = require('../controller/koperasi/laporanController');
+const pinjamanController = require('../controller/koperasi/pinjamanController');
+const simpananController = require('../controller/koperasi/simpananController');
+const kreditController = require('../controller/koperasi/kreditController');
+const masterController = require('../controller/master/');
+
+// Route untuk Login
+routers.get("/login", loginController.getLogin);
+router.post('/login', loginController.login);
+router.post('/logout', loginController.logout);
+
+// Route untuk Laporan
+router.get('/laporan', laporanController.getLaporan);
+router.post('/laporan', laporanController.createLaporan);
+
+// Route untuk Pinjaman
+router.get('/pinjaman', pinjamanController.getPinjaman);
+router.post('/pinjaman', pinjamanController.createPinjaman);
+
+// Route untuk Simpanan
+router.get('/simpanan', simpananController.getSimpanan);
+router.post('/simpanan', simpananController.createSimpanan);
+
+// Route untuk Kredit
+router.get('/kredit', kreditController.getKredit);
+router.post('/kredit', kreditController.createKredit);
+
+// Route untuk Master
+router.get('/master/anggota', masterController.getAnggota);
+router.get('/master/user', masterController.getUser );
+
+// Export Router
 module.exports = router;

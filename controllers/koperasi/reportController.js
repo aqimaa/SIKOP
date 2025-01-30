2f8345bd4d57b87da2b8c6ff6f275039bffaaac9
ppaizzz <faizaldzikro14032005@gmail.com>
2025-01-30 16:00:43 +0700

    zz

--- controllers/koperasi/reportController.js
+++ controllers/koperasi/reportController.js
@@ -1,80 +1,76 @@
 const db = require('../config/database');
 
-const reportController = {
-    getLaporanKredit: async (req, res) => {
-        try {
-            const { tahun, bulan, jenis } = req.query;
-            let query = `
-                SELECT a.nip, a.nama, k.jangka_waktu, k.total_angsuran, k.jumlah_pinjaman as jumlah 
-                FROM anggota a 
-                JOIN kredit_${jenis.toLowerCase()} k ON a.id = k.id_anggota
-                WHERE YEAR(k.tanggal_mulai) = ? AND MONTH(k.tanggal_mulai) = ?
-            `;
-            
-            const [data] = await db.execute(query, [tahun, bulan]);
-            
-            res.render('reportTemplate', {
-                jenis: 'Kredit',
-                jenisDetail: jenis,
-                tahun,
-                bulan,
-                data
-            });
-        } catch (error) {
-            res.status(500).send(error.message);
+exports.getLaporanKredit = (req, res) => {
+    const { tahun, bulan, jenis } = req.query;
+    const query = `
+        SELECT a.nip, a.nama, k.jangka_waktu, k.total_angsuran, k.jumlah_pinjaman as jumlah 
+        FROM anggota a 
+        JOIN kredit_${jenis.toLowerCase()} k ON a.id = k.id_anggota
+        WHERE YEAR(k.tanggal_mulai) = ? AND MONTH(k.tanggal_mulai) = ?
+    `;
+    
+    db.query(query, [tahun, bulan], (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: 'Database error', error: err });
         }
-    },
 
-    getLaporanPinjaman: async (req, res) => {
-        try {
-            const { tahun, bulan, kategori } = req.query;
-            let query = `
-                SELECT a.nip, a.nama, p.jangka_waktu, p.total_angsuran, p.jumlah_pinjaman as jumlah 
-                FROM anggota a 
-                JOIN pinjaman p ON a.id = p.id_anggota
-                WHERE YEAR(p.tanggal_perjanjian) = ? AND MONTH(p.tanggal_perjanjian) = ?
-                AND p.kategori = ?
-            `;
-            
-            const [data] = await db.execute(query, [tahun, bulan, kategori]);
-            
-            res.render('reportTemplate', {
-                jenis: 'Pinjaman',
-                jenisDetail: kategori,
-                tahun,
-                bulan,
-                data
-            });
-        } catch (error) {
-            res.status(500).send(error.message);
-        }
-    },
-
-    getLaporanSimpanan: async (req, res) => {
-        try {
-            const { tahun, bulan, jenis } = req.query;
-            const jenisColumn = `simpanan_${jenis.toLowerCase()}`;
-            
-            let query = `
-                SELECT a.nip, a.nama, s.${jenisColumn} as jumlah 
-                FROM anggota a 
-                JOIN simpanan s ON a.id = s.id_anggota
-                WHERE YEAR(s.tanggal) = ? AND MONTH(s.tanggal) = ?
-            `;
-            
-            const [data] = await db.execute(query, [tahun, bulan]);
-            
-            res.render('reportTemplate', {
-                jenis: 'Simpanan',
-                jenisDetail: jenis,
-                tahun,
-                bulan,
-                data
-            });
-        } catch (error) {
-            res.status(500).send(error.message);
-        }
-    }
+        res.render('reportTemplate', {
+            jenis: 'Kredit',
+            jenisDetail: jenis,
+            tahun,
+            bulan,
+            data: results
+        });
+    });
 };
 
-module.exports = reportController;
+exports.getLaporanPinjaman = (req, res) => {
+    const { tahun, bulan, kategori } = req.query;
+    const query = `
+        SELECT a.nip, a.nama, p.jangka_waktu, p.total_angsuran, p.jumlah_pinjaman as jumlah 
+        FROM anggota a 
+        JOIN pinjaman p ON a.id = p.id_anggota
+        WHERE YEAR(p.tanggal_perjanjian) = ? AND MONTH(p.tanggal_perjanjian) = ?
+        AND p.kategori = ?
+    `;
+    
+    db.query(query, [tahun, bulan, kategori], (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: 'Database error', error: err });
+        }
+
+        res.render('reportTemplate', {
+            jenis: 'Pinjaman',
+            jenisDetail: kategori,
+            tahun,
+            bulan,
+            data: results
+        });
+    });
+};
+
+exports.getLaporanSimpanan = (req, res) => {
+    const { tahun, bulan, jenis } = req.query;
+    const jenisColumn = `simpanan_${jenis.toLowerCase()}`;
+    
+    const query = `
+        SELECT a.nip, a.nama, s.${jenisColumn} as jumlah 
+        FROM anggota a 
+        JOIN simpanan s ON a.id = s.id_anggota
+        WHERE YEAR(s.tanggal) = ? AND MONTH(s.tanggal) = ?
+    `;
+    
+    db.query(query, [tahun, bulan], (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: 'Database error', error: err });
+        }
+
+        res.render('reportTemplate', {
+            jenis: 'Simpanan',
+            jenisDetail: jenis,
+            tahun,
+            bulan,
+            data: results
+        });
+    });
+};

77ff73cc50e9ede4baa7b73bcdd7ce51ef2cf55f
ppaizzz <faizaldzikro14032005@gmail.com>
2025-02-03 11:38:36 +0700

    Revert "Merge branch 'ari' ini simpanan yang benar"
    This reverts commit e4afe080f8e1eef54c13e4d643b943947fbdfe75, reversing
    changes made to e40afa2bc1576e1783d2075ae2743eedd6c8fb90.

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,11 +4,6 @@ const db = require('../config/database');
 
 // Import Controllers
 const loginController = require("../controllers/auth/loginController.js");
-// const laporanController = require('../controllers/koperasi/laporanController');
-// const pinjamanController = require('../controllers/koperasi/pinjamanController');
-const simpananController = require('../controllers/koperasi/simpananController.js');
-// const kreditController = require('../controllers/koperasi/kreditController');
-// const masterController = require('../controllers/master/masterController');
 const simpananController = require('../controllers/koperasi/simpananController');
 const pinjamanController = require('../controllers/koperasi/pinjamanController');
 const kreditController = require('../controllers/koperasi/kreditController');
@@ -119,43 +114,6 @@ router.post('/kredit/filter', async (req, res) => {
 });
 
 
-// Route untuk Pinjaman
-// router.get('/pinjaman', pinjamanController.getPinjaman);
-// router.post('/pinjaman', pinjamanController.createPinjaman);
-
-// Route untuk Simpanan
-router.get('/simpanan', (req, res) => {
-  if (req.session.role === 'Admin Keuangan') {
-      res.render('koperasi/simpananKeuangan/lihatsimpanan');
-  } else {
-      res.redirect('/login');
-  }
-});
-
-// API Routes untuk Simpanan
-router.get('/api/simpanan', simpananController.getSimpananData);
-router.get('/koperasi/simpanan/lihatsimpanan', simpananController.lihatSimpanan);
-
-// API Routes untuk Simpanan
-router.get('/api/simpanan', simpananController.getSimpananData);
-
-router.get('/api/simpanan/filter', simpananController.filterSimpanan);
-router.get('/api/simpanan/years', simpananController.getAvailableYears);
-router.get('/api/anggota', simpananController.getAnggotaList);
-router.post('/api/simpanan', simpananController.createSimpanan);
-router.delete('/api/simpanan/:id', simpananController.deleteSimpanan);
-router.get('/api/simpanan/history/:id_anggota', simpananController.getHistorySimpanan);
-
-
-// Route untuk Kredit
-// router.get('/kredit', kreditController.getKredit);
-// router.post('/kredit', kreditController.createKredit);
-
-// Route untuk Master
-// router.get('/master/anggota', masterController.getAnggota);
-// router.get('/master/user', masterController.getUser);
-
-// Export Router
 // Route untuk Pinjaman Pimpinan
 router.get('/pinjamanPimpinan', pinjamanController.getPinjamanPimpinan);
 router.post('/pinjaman/filter', async (req, res) => {

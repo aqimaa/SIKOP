201a659d3203dbab7450ebe61bf4e83f8d6eca8a
Meydiva-IY <meydivaintayeza@gmail.com>
2025-02-12 15:52:04 +0700

    Semua Dashboard Mey Fix

--- controllers/master/masterController.js
+++ controllers/master/masterController.js
@@ -4,38 +4,43 @@ const db = require('../../config/database');
 
 exports.getDashboardData = (req, res) => {
   const queryPegawai = 'SELECT COUNT(*) AS total_pegawai FROM pegawai';
-  const queryAnggota = 'SELECT COUNT(*) AS total_anggota FROM anggota';
+  const queryAnggotaAktif = "SELECT COUNT(*) AS total_anggota_aktif FROM anggota WHERE status = 'Aktif'";
+  const queryAnggotaTidakAktif = "SELECT COUNT(*) AS total_anggota_tidak_aktif FROM anggota WHERE status = 'Tidak Aktif'";
   const querySimpanan = 'SELECT SUM(simpanan_wajib + simpanan_pokok + simpanan_sukarela) AS total_simpanan FROM simpanan';
   const queryPinjaman = 'SELECT SUM(jumlah_pinjaman) AS total_pinjaman FROM pinjaman';
   const queryKredit = `
-    SELECT SUM(harga_pokok) AS total_kredit FROM kredit_barang 
-    UNION ALL 
-    SELECT SUM(jumlah_pinjaman) FROM kredit_motor 
-    UNION ALL 
-    SELECT SUM(jumlah_pinjaman) FROM kredit_elektronik 
-    UNION ALL 
-    SELECT SUM(jumlah_pinjaman) FROM kredit_umroh
+      SELECT SUM(harga_pokok) AS total_kredit FROM kredit_barang 
+      UNION ALL 
+      SELECT SUM(jumlah_pinjaman) FROM kredit_motor 
+      UNION ALL 
+      SELECT SUM(jumlah_pinjaman) FROM kredit_elektronik 
+      UNION ALL 
+      SELECT SUM(jumlah_pinjaman) FROM kredit_umroh
   `;
 
   db.query(queryPegawai, (err, pegawaiResult) => {
       if (err) throw err;
-      db.query(queryAnggota, (err, anggotaResult) => {
+      db.query(queryAnggotaAktif, (err, anggotaAktifResult) => {
           if (err) throw err;
-          db.query(querySimpanan, (err, simpananResult) => {
+          db.query(queryAnggotaTidakAktif, (err, anggotaTidakAktifResult) => {
               if (err) throw err;
-              db.query(queryPinjaman, (err, pinjamanResult) => {
+              db.query(querySimpanan, (err, simpananResult) => {
                   if (err) throw err;
-                  db.query(queryKredit, (err, kreditResult) => {
+                  db.query(queryPinjaman, (err, pinjamanResult) => {
                       if (err) throw err;
+                      db.query(queryKredit, (err, kreditResult) => {
+                          if (err) throw err;
 
-                      const totalKredit = kreditResult.reduce((acc, curr) => acc + (curr.total_kredit || 0), 0);
+                          const totalKredit = kreditResult.reduce((acc, curr) => acc + (curr.total_kredit || 0), 0);
 
-                      res.json({
-                          total_pegawai: pegawaiResult[0].total_pegawai,
-                          total_anggota: anggotaResult[0].total_anggota,
-                          total_simpanan: simpananResult[0].total_simpanan,
-                          total_pinjaman: pinjamanResult[0].total_pinjaman,
-                          total_kredit: totalKredit
+                          res.json({
+                              total_pegawai: pegawaiResult[0].total_pegawai,
+                              total_anggota_aktif: anggotaAktifResult[0].total_anggota_aktif,
+                              total_anggota_tidak_aktif: anggotaTidakAktifResult[0].total_anggota_tidak_aktif,
+                              total_simpanan: simpananResult[0].total_simpanan,
+                              total_pinjaman: pinjamanResult[0].total_pinjaman,
+                              total_kredit: totalKredit
+                          });
                       });
                   });
               });
